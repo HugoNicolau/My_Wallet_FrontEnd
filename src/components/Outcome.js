5e440b0ffcc52102817dbf51f029dc50e70c8d49
HugoNicolau <nicolau.hugogiles@gmail.com>
2022-11-21 04:45:15 -0300

    outcome route layout

---
+++ src/components/Outcome.js
@@ -0,0 +1,90 @@
+import styled from "styled-components";
+
+
+export default function Outcome(){
+
+    return(
+        <Container>
+            <HeaderContainer>
+                Nova saída
+            </HeaderContainer>
+            <Field placeholder="Valor"/>
+            <Field placeholder="Descrição"/>
+            <StyledButton>Salvar saída</StyledButton>
+        </Container>
+    )
+}
+
+
+
+const Container = styled.div`
+
+background-color: #8c11be;
+width:100vw;
+height:100vh;
+display:flex;
+flex-direction:column;
+padding-left: 25px;
+padding-right:25px;
+padding-top:25px;
+padding-bottom:16px;
+
+`
+const HeaderContainer = styled.div`
+font-family: Raleway;
+font-size: 26px;
+font-weight: 700;
+line-height: 31px;
+letter-spacing: 0em;
+text-align: left;
+color:#ffffff;
+display:flex;
+justify-content:space-between;
+margin-bottom:40px;
+`
+
+const Field = styled.input`
+height: 58px;
+width:100%;
+border-radius: 5px;
+font-family: 'Raleway';
+font-size: 20px;
+font-weight: 400;
+line-height: 23px;
+letter-spacing: 0em;
+text-align: left;
+padding:15px;
+margin-bottom:13px;
+box-shadow:none;
+border-style:solid;
+border:none;
+&::placeholder{
+color: #000000;
+}
+`
+
+const StyledButton = styled.button`
+background-color: #A328D6;
+height: 46px;
+width: 326px;
+left: 23px;
+top: 375px;
+border-radius: 5px;
+color:#ffffff;
+font-family: 'Raleway';
+font-size: 20px;
+font-weight: 700;
+line-height: 23px;
+letter-spacing: 0em;
+text-align: left;
+margin-bottom:36px;
+display:flex;
+align-items:center;
+justify-content:center;
+box-shadow:none;
+border-style:solid;
+border-color:#A328D6;
+
+:active{
+    background-color: #b44de0;
+}`
